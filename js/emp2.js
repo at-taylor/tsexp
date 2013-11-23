9075a954e1f62ecfdfcfb103ff242ba9d288372e
Tim Parmer <tim.parmer@outremersystemdesign.com>
2013-11-23 18:45:18 -0500

    story library

--- js/emp2.js
+++ js/emp2.js
@@ -50,19 +50,19 @@ $('#employeeListPage').bind('pagechange', function(event) {
 });
 
 function getEmployeeList() {
-   // alert('running list()')        ;
+    // alert('running list()')        ;
 
-	$.getJSON('getemployees.html', function(data) {
-            /*$.getJSON(serviceURL + 'getemployees.html', function(data) {*/
-		$('#employeeList li').remove();
-		employees = data.items;
-		$.each(employees, function(index, employee) {
-			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
-					'<img src="pics/' + employee.picture + '"/>' +
-					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
-					'<p>' + employee.title + '</p>' +
-					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
-		});
-		$('#employeeList').listview('refresh');
-	});
+    $.getJSON('getemployees.html', function(data) {
+        /*$.getJSON(serviceURL + 'getemployees.html', function(data) {*/
+        $('#employeeList li').remove();
+        employees = data.items;
+        $.each(employees, function(index, employee) {
+            $('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+                '<img src="pics/' + employee.picture + '"/>' +
+                '<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+                '<p>' + employee.title + '</p>' +
+                '<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+        });
+        $('#employeeList').listview('refresh');
+    });
 }
--- js/storylist.js
+++ js/storylist.js
@@ -1,41 +1,83 @@
 var serviceURL = "http://localhost/emp/services/";
 
+var employees;
 var stories;
 
-$('#storyListPage').bind('pagebeforecreate', function(event) {
+$('#employeeListPage').bind('pagebeforecreate', function(event) {
     alert('bind: pagebeforecreate');
-    getStoryList();
+    //getEmployeeList();
 });
-$('#storyListPage').bind('pagecreate', function(event) {
+$('#employeeListPage').bind('pagecreate', function(event) {
     alert('bind: pagecreate');
-    getStoryList();
+    //getEmployeeList();
 });
 
-$('#storyListPage').bind('pageinit', function(event) {
+$('#employeeListPage').bind('pageinit', function(event) {
     alert('bind: pageinit');
-    getStoryList();
+    getEmployeeList();
 });
 
-$('#storyListPage').live('pageinit', function(event) {
+$('#employeeListPage').live('pageinit', function(event) {
     alert('live: pageinit');
-    getStoryList();
+    getEmployeeList();
+});
+$('#employeeListPage').bind('pagebeforeload', function(event) {
+    alert('bind: pagebeforeload');
+    //getEmployeeList();
 });
 
-function getStoryList() {
-     //alert('running list()')        ;
+$('#employeeListPage').bind('pageload', function(event) {
+    alert('bind: pageload');
+    //getEmployeeList();
+});
 
-    $.getJSON('getstorylist.html', function(data) {
+$('#employeeListPage').bind('pagebeforeshow', function(event) {
+    alert('bind: pagebeforeshow');
+    getEmployeeList();
+});
+
+$('#employeeListPage').bind('pageshow', function(event) {
+    alert('bind: pageshow');
+    //getEmployeeList();
+});
+$('#employeeListPage').bind('pagebeforechange', function(event) {
+    alert('bind: pagebeforeshow');
+    //getEmployeeList();
+});
+
+$('#employeeListPage').bind('pagechange', function(event) {
+    alert('bind: pageshow');
+    //getEmployeeList();
+});
+
+function getEmployeeList() {
+   // alert('running list()')        ;
+        $.getJSON('getstorylist.html', function(data) {
         /*$.getJSON(serviceURL + 'getemployees.html', function(data) {*/
-        $('#storyList li').remove();
+        $('#employeeList li').remove();
         stories = data.items;
         $.each(stories, function(index, story) {
-            $('#storyList').append('<li><a href="storydetails.html?id=' + story.id + '">' +
+            $('#employeeList').append('<li><a href="storydetails.html?id=' + story.id + '">' +
                 '<h4>' + story.title + '</h4>' +
                 '<p>' + story.storydate + '</p>' +
                 '<p>' + story.categories + '</p>' +
                 '<p>' + story.status + '</p>' +
                 '</a></li>');
         });
-        $('#storyList').listview('refresh');
+        $('#employeeList').listview('refresh');
     });
+
+//	$.getJSON('getemployees.html', function(data) {
+//            /*$.getJSON(serviceURL + 'getemployees.html', function(data) {*/
+//		$('#employeeList li').remove();
+//		employees = data.items;
+//		$.each(employees, function(index, employee) {
+//			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+//					'<img src="pics/' + employee.picture + '"/>' +
+//					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+//					'<p>' + employee.title + '</p>' +
+//					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+//		});
+//		$('#employeeList').listview('refresh');
+//	});
 }
