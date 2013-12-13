08efbe8c0da39d543de25e7e1713f66800f6baad
Tim Parmer <tim.parmer@outremersystemdesign.com>
2013-12-13 17:43:55 -0500

    audio test 5

--- js/addmedia.js
+++ js/addmedia.js
@@ -80,7 +80,7 @@ var app = {
 // document.getElementById('whitelist').addEventListener('click', this.whitelist, false);
         document.getElementById('camera').addEventListener('click', this.camera, false);
         document.getElementById('video').addEventListener('click', this.captureVideo, false);
-        document.getElementById('audio').addEventListener('click', this.recordAudio, false);
+        document.getElementById('audio').addEventListener('click', this.captureAudio, false);
         document.getElementById('getphoto').addEventListener('click', this.getphoto, false);
         alert('after binding events');
     },
